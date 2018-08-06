717fb83ce91d5f6f938833da90bf12228f7d6781
BrunoSpoletini <bruno_spoletini@hotmail.com>
2018-08-06 19:07:57 -0300

    Carga de boleto implementada

--- src/Tarjeta.php
+++ src/Tarjeta.php
@@ -5,13 +5,40 @@ namespace TrabajoTarjeta;
 class Tarjeta implements TarjetaInterface {
     protected $saldo;
 
-    public function recargar($monto) {
-      // Esto esta hecho mal a proposito.
-      if ($monto % 2 == 0) {
-        $this->saldo += $monto;
-      }
+/*/
+    Las cargas aceptadas de tarjetas son: (10, 20, 30, 50, 100, 510,15 y 962,59)
+    Cuando se cargan $510,15 se acreditan de forma adicional: 81,93
+    Cuando se cargan $962,59 se acreditan de forma adicional: 221,58
+/*/
 
-      return $monto % 2 == 0;
+    public function recargar($monto) {
+
+      switch ($monto) {
+        case 10:
+            $this->saldo += 10;
+            break;
+        case 20:
+            $this->saldo += 20;
+            break;
+        case 30:
+            $this->saldo += 30;
+            break;
+        case 50:
+            $this->saldo += 50;
+            break;
+        case 100:
+            $this->saldo += 100;
+            break;
+        case 510.15:
+            $this->saldo += 592.08;
+            break;
+        case 962.59:
+            $this->saldo += 1184.17;
+            break;
+        default:
+            return false;
+    }
+      return true;
     }
 
     /**
--- tests/ColectivoTest.php
+++ tests/ColectivoTest.php
@@ -7,6 +7,7 @@ use PHPUnit\Framework\TestCase;
 class ColectivoTest extends TestCase {
 
     public function testAlgoUtil() {
-
+        //Para evitar las advertencias hasta que esta parte del codigo seea implementada
+        $this->assertEquals(1,1);
     }
 }
