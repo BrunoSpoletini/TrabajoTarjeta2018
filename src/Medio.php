5684f7eb566a45b5a14a119bdad4257cae497763
BrunoSpoletini <bruno_spoletini@hotmail.com>
2018-08-24 09:42:20 -0300

    Corrección de sobreposicion de branches

---
+++ src/Medio.php
@@ -0,0 +1,19 @@
+<?php
+namespace TrabajoTarjeta;
+//Escribir un test que valide que una tarjeta de FranquiciaCompleta siempre puede pagar un boleto.
+//Escribir un test que valide que el monto del boleto pagado con medio boleto es siempre la mitad del normal.
+/*/
+Tarjeta medio
+Tarjeta completo
+/*/
+class Medio extends Tarjeta {
+    public function restarSaldo($valorB){
+        if($this->saldo<($valorB/2)){
+                return FALSE;
+        }
+        else{
+                $this->saldo-=($valorB/2);
+                return TRUE;
+            }
+        }
+}
---
+++ tests/MedioTest.php
@@ -0,0 +1,28 @@
+<?php
+namespace TrabajoTarjeta;
+use PHPUnit\Framework\TestCase;
+class MedioTest extends TestCase {
+    /**
+     * Comprueba que la tarjeta con media franquicia no puede cargar saldos invalidos.
+     */
+    public function testCargaSaldoInvalido() {
+      $medio = new Medio;
+      $this->assertFalse($medio->recargar(15));
+      $this->assertEquals($medio->obtenerSaldo(), 0);
+  }
+  public function testRestarBoletos() {
+        $medio = new Medio;
+        $this->assertTrue($medio->recargar(20));
+        $this->assertEquals($medio->obtenerSaldo(), 20);
+        $this->assertEquals($medio->restarSaldo(14.8), TRUE);
+        $this->assertEquals($medio->obtenerSaldo(), 12.6);
+        $this->assertEquals($medio->restarSaldo(14.8), TRUE);
+        $this->assertEquals($medio->obtenerSaldo(), 5.2);
+        $this->assertEquals($medio->restarSaldo(14.8), FALSE);
+        $this->assertTrue($medio->recargar(962.59));
+        for( ($i = 0);$i<160;++$i){
+            $this->assertEquals($medio->restarSaldo(14.8), TRUE);
+        }
+        $this->assertEquals($medio->restarSaldo(14.8), FALSE);
+  }
+}
