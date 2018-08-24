3dbc494000629e6edfb2eeb0ca3fa9c10554c532
BrunoSpoletini <bruno_spoletini@hotmail.com>
2018-08-24 10:10:22 -0300

    Arreglo infimo

---
+++ src/Completo.php
@@ -0,0 +1,13 @@
+<?php
+namespace TrabajoTarjeta;
+//Escribir un test que valide que una tarjeta de FranquiciaCompleta siempre puede pagar un boleto.
+//Escribir un test que valide que el monto del boleto pagado con medio boleto es siempre la mitad del normal.
+/*/
+Tarjeta medio
+Tarjeta completo
+/*/
+class Completo extends Tarjeta {
+    public function restarSaldo($valorB){
+        return TRUE;
+        }
+}
