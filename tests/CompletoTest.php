83b57258ca837369405d4941ddb08aeb86a912b2
Lautaro Cerruti <lautarocerruti7@gmail.com>
2018-08-26 21:28:48 -0300

    Completo test pruebas inecesarias

--- tests/CompletoTest.php
+++ tests/CompletoTest.php
@@ -5,21 +5,8 @@ class CompletoTest extends TestCase {
     /**
      * Comprueba que la tarjeta con media franquicia no puede cargar saldos invalidos.
      */
-    public function testCargaSaldoInvalido() {
-      $completo = new Completo;
-      $this->assertFalse($completo->recargar(15));
-      $this->assertEquals($completo->obtenerSaldo(), 0);
-  }
   public function testRestarBoletos() {
         $completo = new Completo;
-        $this->assertTrue($completo->recargar(20));
-        $this->assertEquals($completo->obtenerSaldo(), 20);
-        $this->assertEquals($completo->restarSaldo(14.8), TRUE);
-        $this->assertEquals($completo->obtenerSaldo(), 20);
-        $this->assertEquals($completo->restarSaldo(14.8), TRUE);
-        $this->assertEquals($completo->obtenerSaldo(), 20);
-        $this->assertEquals($completo->restarSaldo(14.8), TRUE);
-        $this->assertTrue($completo->recargar(962.59));
         for( ($i = 0);$i<160;++$i){
             $this->assertEquals($completo->restarSaldo(14.8), TRUE);
         }
